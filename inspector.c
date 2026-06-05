43b1da194f0c2af9be4ea19d1ec125b37a13f5b2
tommrot <tommasorotelli12@gmail.com>
2026-06-05 15:09:13 +0200

    implemented floar informations

--- inspector.c
+++ inspector.c
@@ -6,6 +6,7 @@ void take_data(char **argv);
 void print_bits(void *data, size_t size);
 void print_hex(void *data, size_t size);
 void print_addresses(void *data, size_t size);
+void print_float_informations(float *data, size_t size);
 
 int main(int argc, char **argv){
     if (strcmp(argv[1], "int") != 0 && strcmp(argv[1], "float") != 0 && strcmp(argv[1], "char") != 0 && strcmp(argv[1], "string") != 0){
@@ -27,13 +28,14 @@ void take_data(char **argv){
         size = sizeof(float);
         data_p = malloc(size);
         *(float*)data_p = atof(argv[2]);
-    } else if (strcmp(argv[1], "char")){
+        print_float_informations((float*) data_p, size);
+    } else if (strcmp(argv[1], "char") == 0){
         size = sizeof(char);
         data_p = malloc(size);
         *(char*)data_p = *(argv[2]);
-    } else if (strcmp(argv[1], "string")){
+    } else if (strcmp(argv[1], "string") == 0){
         size = strlen(argv[2]);
-        data_p = malloc(size);
+        data_p = malloc(size) + 1;
         strcpy((char*)data_p, argv[2]);
     }
     print_bits(data_p, size);
@@ -43,7 +45,6 @@ void take_data(char **argv){
 
 
 void print_bits(void *data, size_t size){
-    printf("string len: %ld", size);
     printf("bit representation:\n");
     char *p = (char*) data;
     for (int i = size - 1; i >= 0; i--){
@@ -74,4 +75,36 @@ void print_addresses(void *data, size_t size){
         printf("%p\n", (void*)&p[i]); // or (void*)(p + 1). we use (void*) because %p is pointer placeholder and it expects generic pointer
     }
     printf("\n");
+}
+
+
+void print_float_informations(float *data, size_t size){
+    char *p = (char *) data;
+    for (int i = size - 1; i >= 0; i--){
+        if (i == size - 1){
+            for (int j = 7; j >= 0; j--){
+                int bit = (p[i] >> j) & 1;
+                if (j == 7){
+                    printf("sign: %d\n\nexponent:\n", bit);
+                } else {
+                    printf("%d", bit);
+                }
+            }
+        } else if (i == size - 2){
+            for (int j = 7; j >= 0; j--){
+                int bit = (p[i] >> j) & 1;
+                if (j == 7){
+                    printf("%d\n\nmantix:\n", bit);
+                } else {
+                    printf("%d", bit);
+                }
+            }
+        } else {
+            for (int j = 7; j >= 0; j--){
+                int bit = (p[i] >> j) & 1;
+                printf("%d", bit);
+            }
+        }
+    }
+    printf("\n\n");
 }
